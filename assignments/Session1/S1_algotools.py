aa70743e9d462856492c217143ba4886f98ca5b9
StephLaudou <stephanie.laudou@gmail.com>
2018-10-07 15:32:15 +0200

    HW Session1 BoundingBox

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -4,6 +4,11 @@
 
 Ceci est un script temporaire.
 """
+
+#Bounding Box
+import numpy as np
+
+
 #Average
 def average_above_zero(tab):
     """
@@ -41,12 +46,12 @@ def average_above_zero(tab):
 
 
 #Test fonction average    
-test_tab=[1,2,3,-8] #¯ decl liste
-moy=average_above_zero(test_tab)
-print('Positive valus average =')
-print(moy)# print reçoit un bloc quidoit être d etype homogène
-print('Positive valus average ='+str(moy)) # methode moche
-print('Positive valus average ={v}'.format(v=moy)) # v = mot clé
+#test_tab=[1,2,3,-8] #¯ decl liste
+#moy=average_above_zero(test_tab)
+#print('Positive valus average =')
+#print(moy)# print reçoit un bloc quidoit être d etype homogène
+#print('Positive valus average ='+str(moy)) # methode moche
+#print('Positive valus average ={v}'.format(v=moy)) # v = mot clé
 
 
 #Maximum value
@@ -118,3 +123,51 @@ def reverse_table(tab):
 #test_tab=[1,2,15,9] #¯ decl liste
 #test_tab=[] #¯ decl liste
 #print(reverse_table(test_tab))
+    
+
+#Bounding Box
+def roi_bbox(image):
+    """
+    Brief : return an array which contains the coordinates of the bounding box of an image    
+    
+    Args : image : numpy array, 2D Matrix with binary values
+    
+    Return : numpy array which contains following coordinates  : top left, top right, bottom left, bottom right
+        
+    Raises : 
+    
+    
+    """
+    #coordonnées des points angle de la matrice / Initialisation avec la taille de l'image
+    #valeur des points transposée pour que la boucle marche
+    #Point en haut à gauche 
+    xHG=len(image[0])-1#nb de colonnes à la première ligne POURQUOI -1 FIXME
+    yHG=len(image)-1#nb de ligne de l'image
+    #Point en bas à droite
+    xBD=0#colonne
+    yBD=0#ligne
+    
+    #je parcours les cases du tableau et analyse celles = 1 (si 1, elle appartient à l'image)
+    for nlig in range(len(image)):
+        for ncol in range (len(image[0])):
+            if image[nlig][ncol] == 1:
+                if nlig<yHG:
+                    yHG=nlig
+                if nlig>yBD:
+                    yBD=nlig
+                if ncol<xHG:
+                    xHG=ncol
+                if ncol>xBD:
+                    xBD=ncol
+    
+    arrayCoordinates=np.array([[xHG,yHG],[xBD,yHG],[xHG,yBD],[xBD,yBD]])
+
+    return arrayCoordinates
+
+#Tests bounding box
+#image = np.array([[0,0,0,0,0,0],[0,0,1,1,1,0],[0,0,1,1,0,0],[0,0,0,1,1,0],[0,0,0,0,0,0]]) #¯ decl liste
+#print(image)
+#print(image[1][3])#valeur de la case du tableau à la 2eme ligne (1ere ligne =0) et 3eme colonne
+#print(len(image[0]))# nb de colonnes de la ligne 1
+#print(len(image))# nb de lignes de l'image
+#print(roi_bbox(image))
