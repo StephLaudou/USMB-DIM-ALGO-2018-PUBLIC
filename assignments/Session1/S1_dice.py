f554d4f5e126f4b680e87e55b7efe940653c8d65
StephLaudou <stephanie.laudou@gmail.com>
2018-10-15 23:13:44 +0200

    DiceGame PrettyFinished

--- assignments/Session1/S1_dice.py
+++ assignments/Session1/S1_dice.py
@@ -25,61 +25,54 @@ def yes_no(question):
 player=1  # 1=user, 2=computer // je commence par le player 1
 userScore = 0
 computerScore = 0
-turnScore = 0
-dice=0
-winScore=7
+winScore=100
 result=''
-answer=True
-turn=True
-turnScore = 0 
-computerLimit=10
+computerLimit=14
+print("Au tour du joueur")
 
 while (userScore<winScore and computerScore<winScore):    
-    if player == 2 or (userScore==0 and computerScore==0):
-        player = 1
-    else:
-        player = 2
-    
     turn=True
     turnScore = 0      
      
     while turn == True:#on commence un tour
         dice=random.randint(1,6)#on lance le dé.
-        #print(dice)
+        print("Tirage de dé: {}".format(dice))
         if dice != 1:
             turnScore = turnScore + dice
             if player == 1:#si c'est le tour du user   
-                answer = yes_no('Voulez-vous continuer?')
+                answer = yes_no('Voulez-vous continuer? ({})'.format(turnScore))
                 if answer == True:# le joueur a dit qu'il voulait continuer
                     turn=True#le tour continue
                 else:
                    userScore = userScore+turnScore
                    turn=False
+                   
                  
             else:#si c'est le tour du computer                         
-                if turnScore < computerLimit :
+                if turnScore < computerLimit and (winScore-computerScore-turnScore)>0:
                     turn=True
                 else:
                     computerScore = computerScore+turnScore
                     turn=False
         else:
-            turn =False
-        
-        
+            turn = False
+    print('Points de ordinateur : {}'.format(computerScore))
+    print('Points du joueur : {}'.format(userScore))    
+    
+    if player == 2: 
+        player = 1
+        print("Au tour du Joueur")
+    else:
+        player = 2
+        print("Au tour de l'Ordinateur")
                
 if userScore>computerScore:
-    result = 'le joueur a gagné'
+    result = "Le joueur a gagné : {} points à {} points pour l'ordinateur".format(userScore,computerScore)
 else:
-    result = 'l ordinateur a gagné'
+    result = "L'ordinateur a gagné : {} points à {} points pour le joueur".format(computerScore,userScore)
 print(result)
-print(userScore)
-print(computerScore)
 
 
-
-#print('Le joueur a '+str(userScore)+' points')
-#print('L ordinateur a '+str(computerScore)+' points')         
-#print('Points du joueur'.format(v=moy))            
 
 
 
